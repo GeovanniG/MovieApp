0dedb5bfef06941f6e032848a4fd7d05ba0487a1
GeovanniG <32276348+GeovanniG@users.noreply.github.com>
2019-08-14 12:09:09 -0700

    Add caching and compress files for production

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
-const logger = require('morgan');
+const compression = require('compression');
 
 const app = express();
+const buildDirectoryPath = path.join(__dirname, 'client/build');
+const staticDirectoryPath = path.join(__dirname, 'client/build', 'static');
+const indexDirectoryPath = path.join(__dirname, 'client/build', 'index.html');
 const port = process.env.PORT || '3001';
 
 const userRouter = require('./routers/users');
 const userInfoRouter = require('./routers/userInfo');
 
-app.use(helmet())
-app.use(logger('dev'));
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -20,12 +23,25 @@ app.use('/', userInfoRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(buildDirectoryPath, {
+    setHeaders: (res, path) => {
+        if (path.includes(indexDirectoryPath)) {
+            res.setHeader('Cache-Control', 'no-cache');
+        } else if (path.includes(staticDirectoryPath)) {
+            res.setHeader('Cache-control', 'max-age=31536000');
+        }
+    }
+  }));
     
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(indexDirectoryPath));
   });
+} else {
+  const logger = require('morgan');
+
+  app.use(logger('dev'));
+  app.use(express.static(buildDirectoryPath));
 }
 
 app.listen(port, () => console.log(`Listening on ${port}`));
